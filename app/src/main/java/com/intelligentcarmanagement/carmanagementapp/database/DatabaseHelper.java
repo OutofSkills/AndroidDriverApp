47874b67d59321d99cca255f6a146dcc61134bfb
OutofSkills <56539829+OutofSkills@users.noreply.github.com>
2022-02-19 20:57:58 +0200

    saving collected sensor data into sqlite db

---
+++ app/src/main/java/com/intelligentcarmanagement/carmanagementapp/database/DatabaseHelper.java
@@ -0,0 +1,115 @@
+package com.intelligentcarmanagement.carmanagementapp.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.intelligentcarmanagement.carmanagementapp.models.Motion;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    // database details
+    private static final String DB_NAME = "carmng.db";
+    private static final int DB_VERSION = 1;
+
+    // Motion table details
+    private static final String MOTION_TABLE = "Motion";
+    private static final String COLUMN_ID = "Id";
+    private static final String COLUMN_ACC_X = "AccX";
+    private static final String COLUMN_ACC_Y = "AccY";
+    private static final String COLUMN_ACC_Z = "AccZ";
+    private static final String COLUMN_GYRO_X = "GyroX";
+    private static final String COLUMN_GYRO_Y = "GyroY";
+    private static final String COLUMN_GYRO_Z = "GyroZ";
+    private static final String COLUMN_TIME_STAMP = "TimeStamp";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    // this is called the first time the database is accessed.
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createMotionTable = "CREATE TABLE " + MOTION_TABLE + "("+ COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_ACC_X + " REAL, " + COLUMN_ACC_Y + " REAL, " + COLUMN_ACC_Z + " REAL, " +
+                COLUMN_GYRO_X + " REAL, " + COLUMN_GYRO_Y + " REAL, " + COLUMN_GYRO_Z + " REAL, " +
+                COLUMN_TIME_STAMP + " DATETIME DEFAULT CURRENT_TIMESTAMP )";
+
+        db.execSQL(createMotionTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+    }
+
+    public boolean InsertMotion(Motion motion)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_ACC_X, motion.getAccelerometerX());
+        cv.put(COLUMN_ACC_Y, motion.getAccelerometerY());
+        cv.put(COLUMN_ACC_Z, motion.getAccelerometerZ());
+
+        cv.put(COLUMN_GYRO_X, motion.getGyroX());
+        cv.put(COLUMN_GYRO_Y, motion.getGyroY());
+        cv.put(COLUMN_GYRO_Z, motion.getGyroZ());
+
+        long result = db.insert(MOTION_TABLE, null, cv);
+
+        db.close();
+
+        if(result == -1)
+            return false;
+
+        return true;
+    }
+
+    public List<Motion> GetMotionData(int rideId) throws ParseException {
+        List<Motion> returnList = new ArrayList<>();
+
+        String queryString = "SELECT* FROM " + MOTION_TABLE;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(queryString, null);
+
+        if(cursor.moveToFirst())
+        {
+            // loop through the cursor (result set) and create a list of motion objects
+            do {
+                float acc_X = cursor.getFloat(1);
+                float acc_Y = cursor.getFloat(2);
+                float acc_Z = cursor.getFloat(3);
+                float gyro_X = cursor.getFloat(4);
+                float gyro_Y = cursor.getFloat(5);
+                float gyro_Z = cursor.getFloat(6);
+                String sTimeStamp = cursor.getString(7);
+
+                Date timestamp = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").parse(sTimeStamp);
+
+                Motion motion = new Motion(acc_X, acc_Y, acc_Z, gyro_X, gyro_Y, gyro_Z, timestamp);
+                returnList.add(motion);
+            }while (cursor.moveToNext());
+        }
+        else
+        {
+            // failure, we have nothing in the list
+        }
+
+        cursor.close();
+        db.close();
+
+        return returnList;
+    }
+}
---
+++ app/src/main/java/com/intelligentcarmanagement/carmanagementapp/services/MotionCollectorService.java
@@ -0,0 +1,50 @@
+package com.intelligentcarmanagement.carmanagementapp.services;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.intelligentcarmanagement.carmanagementapp.database.DatabaseHelper;
+import com.intelligentcarmanagement.carmanagementapp.models.Motion;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MotionCollectorService {
+    private Context context;
+    private DatabaseHelper dbHelper;
+
+    public MotionCollectorService(){};
+
+    public MotionCollectorService(Context context)
+    {
+        this.context = context;
+        dbHelper = new DatabaseHelper(context);
+    }
+
+    public boolean addMotionRecord(Motion motion)
+    {
+        boolean result = dbHelper.InsertMotion(motion);
+
+        return result;
+    }
+
+    public void displayData()
+    {
+        List<Motion> motionList = new ArrayList<>();
+        try {
+            motionList = dbHelper.GetMotionData(1);
+        } catch (ParseException e) {
+            Log.d("DATA TEST: ", e.getMessage());
+        }
+        for(int i = 0; i < motionList.size(); i++)
+            Log.d("MotionViewModel", "Dataset: " +
+                    " acc X: " + motionList.get(i).getAccelerometerX() +
+                    " acc Y: " + motionList.get(i).getAccelerometerY() +
+                    " acc Z: " + motionList.get(i).getAccelerometerZ() +
+                    " gyro X: " + motionList.get(i).getGyroX() +
+                    " gyro Y: " + motionList.get(i).getGyroY() +
+                    " gyro Z: " + motionList.get(i).getGyroZ()
+            );
+    }
+}

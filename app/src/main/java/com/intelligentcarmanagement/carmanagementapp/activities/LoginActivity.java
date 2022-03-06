94f007f41ae932e65e42cef70f50457d48f3ed8a
OutofSkills <56539829+OutofSkills@users.noreply.github.com>
2022-03-06 14:53:16 +0200

    added login view

---
+++ app/src/main/java/com/intelligentcarmanagement/carmanagementapp/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.intelligentcarmanagement.carmanagementapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.intelligentcarmanagement.carmanagementapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
